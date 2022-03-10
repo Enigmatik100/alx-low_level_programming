a9d276262845f2c6477d4ada0939860865ebe5b4
enigmatik100 <m.p.gangbadja@gmail.com>
2022-03-11 00:29:36 +0100

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,6 +2,6 @@
 int main(void)
 {
 	/*This is the first task check by betty*/
-	puts("Programming is like building a new multilangual puzzle");
+	puts("Programming is like building a multilangual puzzle");
 	return (0);
 }
