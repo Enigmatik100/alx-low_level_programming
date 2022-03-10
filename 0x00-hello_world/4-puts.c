c79c0df8e3d5f2c8fd3b843fb7fab97d36e185c0
enigmatik100 <m.p.gangbadja@gmail.com>
2022-03-10 13:41:31 -0800

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include "stdio.h"
 int main(void){
-	puts("Programming is like building a new multilangual puzzle\n");
+	puts("Programming is like building a new multilangual puzzle");
 	return 0;
 
 }
