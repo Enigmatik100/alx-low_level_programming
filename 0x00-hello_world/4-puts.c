e7d689a64ee197b753266a7a62ad45876c6092d7
enigmatik100 <m.p.gangbadja@gmail.com>
2022-03-11 01:14:32 +0100

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 /**
  * main - The entrypoint of the code
- * @argc: (argument count)The number of strings pointed to by argv
- * @argv: (argument vector) command line argument
- * Return: void
+ * Return: int
  */
-int main(int argc, char *argv[])
+int main(void)
 {
-/* 7 command-line parsing */
+	/*command-line parsing*/
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
