5dcdd2a7a188fead7865920504fcb1d47e5a6162
enigmatik100 <m.p.gangbadja@gmail.com>
2022-03-13 15:20:51 +0100

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point for C program
+ * Return: 0 showing success execution
+ */
+int main(void)
+{
+	int i = 48;
+
+	while (i < 58)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
