405c2093799f8519a206368c8c460d53b07a9db0
enigmatik100 <m.p.gangbadja@gmail.com>
2022-03-17 13:51:58 +0100

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("%d \n", _islower('a'));
+	printf("%d \n", _islower('A'));
+	printf("%d \n", _islower('Z'));
+	printf("%d \n", _islower('z'));
+	printf("%d \n", _islower('Q'));
+	printf("%d \n", _islower('q'));
+	return (0);
+}
