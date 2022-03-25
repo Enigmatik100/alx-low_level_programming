908de4f38486df422ccf63fd7dd29d4b740b0449
enigmatik100 <m.p.gangbadja@gmail.com>
2022-03-25 02:34:58 +0100

    5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * string_toupper - string to uppercase
+ * @s: string
+ * Return: string
+ */
+char *string_toupper(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i]  < 'z' && s[i] > 'a')
+			s[i] = 'A' + s[i] - 'a';
+		i++;
+	}
+	s[i] = '\0';
+	return (s);
+}
