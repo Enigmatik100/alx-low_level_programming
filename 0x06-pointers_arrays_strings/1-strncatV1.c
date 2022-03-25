ca56bbaa0c9738c504995a42ad90f843763e97bd
enigmatik100 <m.p.gangbadja@gmail.com>
2022-03-25 01:32:29 +0100

    bug fixed

--- 0x06-pointers_arrays_strings/1-strncatV1.c
+++ 0x06-pointers_arrays_strings/1-strncatV1.c
@@ -8,27 +8,12 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int i = 0, j = 0, l = 0, limit;
+	int i = 0, l = 0;
 
-	if (dest)
-	{
-		while (dest[i] != '\0')
-			i++;
-	}
-	if (src)
-	{
-		while (src[l] != '\0')
-			l++;
-
-		limit =  n < l ? n : l;
-
-		while (j < limit)
-		{
-			dest[i] = src[j];
-			i++;
-			j++;
-		}
-		dest[i] = '\0';
-	}
+	while (dest[l] != '\0')
+		l++;
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[l + i] = src[i];
+	dest[l + i] = '\0';
 	return (dest);
 }
