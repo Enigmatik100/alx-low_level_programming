9e7e0e3fe5db4c86b47389c099aa462ef0b42694
enigmatik100 <m.p.gangbadja@gmail.com>
2022-04-07 07:29:55 +0200

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - entrypoint of c program
+ * @argc: number of argument
+ * @argv: array of different argument of our function
+ *
+ * Return: 0 for success
+ */
+int main(int argc, char **argv)
+{
+	(void)argv;
+	printf("%d\n", argc);
+	return (0);
+}
