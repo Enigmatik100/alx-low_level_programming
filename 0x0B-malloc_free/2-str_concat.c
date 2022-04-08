7a192375b1405ebe9db8c169a948b50e8841e127
enigmatik100 <m.p.gangbadja@gmail.com>
2022-04-09 00:53:39 +0100

    right amount memory fixed

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,24 +9,22 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	int i = 0, j = 0, lens1 = 0, lens2 = 1;
+	int i = 0, j = 0, lens1 = 0, lens2 = 0, amount = 1;
 	char *s;
 
 	if (s1 != NULL)
 	{
 		while (s1[lens1] != '\0')
 			lens1++;
-		lens1++;
 	}
 
 	if (s2 != NULL)
 	{
 		while (s2[lens2] != '\0')
 			lens2++;
-		lens2++;
 	}
 
-	s = malloc(sizeof(char) * (lens1 + lens2));
+	s = malloc(sizeof(char) * (lens1 + lens2 + amount));
 
 	if (s == NULL)
 		return (NULL);
