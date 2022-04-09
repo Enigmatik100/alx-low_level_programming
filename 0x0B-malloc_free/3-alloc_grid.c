21ad4fd89384fbb97914569189ae40abc5452289
enigmatik100 <m.p.gangbadja@gmail.com>
2022-04-09 01:22:30 +0100

    3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -12,7 +12,7 @@ int **alloc_grid(int width, int height)
 	int i = 0, j = 0;
 	int **a;
 
-	if (width == 0 || height == 0)
+	if (width <= 0 || height <= 0)
 		return (NULL);
 
 	a = malloc(sizeof(int) * height);
