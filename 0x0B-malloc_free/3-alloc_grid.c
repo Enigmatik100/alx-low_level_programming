1537ddf322321c8f308ab1d5662f1cb88c6e3f07
enigmatik100 <m.p.gangbadja@gmail.com>
2022-04-09 08:21:54 +0100

    bug fixed

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -15,13 +15,13 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	a = malloc(sizeof(int) * height);
+	a = (int**)malloc(sizeof(int*) * height);
 	if (a == NULL)
 		return (NULL);
 
 	for (i = 0; i < height; i++)
 	{
-		a[i] = malloc(sizeof(int) * width);
+		a[i] = (int*)malloc(sizeof(int) * width);
 		if (a[i] == NULL)
 			return (NULL);
 	}
