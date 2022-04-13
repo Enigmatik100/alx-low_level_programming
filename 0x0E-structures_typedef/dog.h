93b5c569f3dcd43bf5c284edb1aedaa2109f4997
enigmatik100 <m.p.gangbadja@gmail.com>
2022-04-13 06:03:34 +0100

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+/**
+ * struct dog - short description of dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of the dog
+ *
+ * Description: Longer description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif /*_ DOG_H_ */
