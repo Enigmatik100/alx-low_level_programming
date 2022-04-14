8c4ec5aca1bbc81b1c5cb6b9a4e6eb7189c0cfa6
enigmatik100 <m.p.gangbadja@gmail.com>
2022-04-14 06:57:13 +0100

    bug fixed

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,4 +1,3 @@
-#include "function_pointers.h"
 #include <stdio.h>
 #include <stdlib.h>
 /**
@@ -10,16 +9,27 @@
  */
 int main(int argc, char *argv[])
 {
+	int i, n;
+
 	if (argc != 2)
 	{
 		printf("Error\n");
 		exit(1);
 	}
-
-	if (atoi(argv[1]) < 0)
+	n = atoi(argv[1]);
+	if (n < 0)
 	{
 		printf("Error\n");
 		exit(2);
+	}
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%02hhx", *((char *)main + i));
+		if (i < n - 1)
+			printf(" ");
+		else
+			printf("\n");
 	}
 	return (0);
 }
