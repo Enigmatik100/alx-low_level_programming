5af02410afe9fece12608acc8760af7fb4016f6c
enigmatik100 <paul.gangbadja@erugis.fr>
2022-04-23 22:11:53 +0100

    1-list_len.c

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * list_len - function that returns the number of element
+ * @h: pointer on list_h
+ *
+ * Return: size_t
+ */
+size_t list_len(const list_t *h)
+{
+	size_t count = 0;
+	const list_t *pt = h;
+	if (pt == NULL)
+		return (count);
+	while (pt != NULL)
+	{
+		count++;
+		pt = pt->next;
+	}
+	return (count);
+}
