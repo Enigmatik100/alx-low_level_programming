bba406dcd810f251a8addb4ad40097e110ae65de
enigmatik100 <paul.gangbadja@erugis.fr>
2022-04-27 05:24:55 +0100

    betty bug fixe

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -14,20 +14,16 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	newNode = malloc(sizeof(listint_t));
 	if (newNode == NULL)
 		return (NULL);
-
 	newNode->n = n;
 	newNode->next = NULL;
-
 	if (*head == NULL)
 	{
 		*head = newNode;
 		return (newNode);
 	}
-	
 	pt = *head;
 	while (pt->next != NULL)
 		pt = pt->next;
-
 	pt->next = newNode;
 	return (newNode);
 }
