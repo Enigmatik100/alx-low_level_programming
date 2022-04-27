c2862c07c7ff6d6c0ee68eebed0467b0841ad13e
enigmatik100 <paul.gangbadja@erugis.fr>
2022-04-27 05:33:41 +0100

    4-free_listint.c

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * free_listint - function that free a linked list
+ * @head: head of the linked list
+ *
+ * Return: nothing
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *current, *next;
+
+	current = head;
+	while (current->next != NULL)
+	{
+		next = current->next;
+		free(current);
+		current = next;
+	}
+	free(current);
+}
