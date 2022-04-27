97f361a270402f3973569490c285b18dc7c38102
enigmatik100 <paul.gangbadja@erugis.fr>
2022-04-27 06:17:58 +0100

    6-pop_listint.c

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * pop_listint - function that deletes the head of a linked list
+ * @head: head of the linked list
+ *
+ * Return: int
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *tmp;
+	int val;
+
+	if (head == NULL)
+		return (0);
+
+	tmp = *head;
+	*head = tmp->next;
+	val = tmp->n;
+	free(tmp);
+	return (val);
+}
