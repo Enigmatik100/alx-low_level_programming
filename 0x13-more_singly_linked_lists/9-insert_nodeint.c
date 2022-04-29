c00f7643bb674e9f4a0d930ab2a0f4a64db5e2b6
enigmatik100 <paul.gangbadja@erugis.fr>
2022-04-29 06:11:02 +0100

    9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -20,7 +20,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		tmp = tmp->next;
 	}
 
-	if (idx > len - 1)
+	if (idx > len)
 		return (NULL);
 
 	newNode = malloc(sizeof(listint_t));
