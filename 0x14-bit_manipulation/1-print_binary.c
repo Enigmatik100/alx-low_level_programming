8074b02c0c1da744232d6109921a42ca18d54837
enigmatik100 <paul.gangbadja@erugis.fr>
2022-05-06 06:36:14 +0100

    1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,27 +7,8 @@
  */
 void print_binary(unsigned long int n)
 {
-	unsigned int mask = 32768;
-	int set = 0;
+	if (n > 1)
+		print_binary(n >> 1);
 
-	if (n == 0)
-		_putchar(0 + '0');
-	else
-	{
-		while (mask > 0)
-		{
-			if ((n & mask) == 0)
-			{
-				if (set != 0)
-					_putchar(0 + '0');
-			}
-			else
-			{
-				set = 1;
-				_putchar(1 + '0');
-			}
-
-			mask = mask >> 1;
-		}
-	}
+	_putchar((n & 1) + '0');
 }
