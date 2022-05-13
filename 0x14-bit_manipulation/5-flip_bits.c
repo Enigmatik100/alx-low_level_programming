0c33033920256a887274bad042b3b858ba24fc24
enigmatik100 <paul.gangbadja@erugis.fr>
2022-05-13 16:42:48 +0100

    5-flip_bits.c

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,23 +1,5 @@
 #include "main.h"
 /**
- * countSetBits - function that counts set bits
- * @n: integer
- *
- * Return: integer
- */
-/*unsigned int countSetBits(unsigned int n)
-{
-	unsigned int count = 0, x = n;
-
-	while (x != 0)
-	{
-		count = count + (x & 1);
-		x = x >> 1;
-	}
-	return (count);
-}
-*/
-/**
  * flip_bits - function taht returns the number of bits to be flipped
  * @n: first integer
  * @m: second integer
