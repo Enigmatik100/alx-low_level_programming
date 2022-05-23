ac2aedf59c7afab05fcf724cb1cc683688369cf4
enigmatik100 <paul.gangbadja@erugis.fr>
2022-05-24 00:16:27 +0100

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,13 +1,11 @@
-#include "holberton.h"
-
+#include "main.h"
 /**
- * _isupper - check for upper case letter
- * @c : character to check the case
- * Return:0 or 1
+ * _isupper - function that cheks for uppercase character
+ * @c: character
+ *
+ * Return: 1 if c is uppercase 0 otherwise
  */
-
 int _isupper(int c)
 {
-	return (c >= 65 && c <= 90);
-
+	return (c <= 90 && c >= 65);
 }
