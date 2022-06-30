88cb2d95709c1de8bb97e84bb67b5861b3495141
enigmatik100 <paul.gangbadja@erugis.fr>
2022-06-30 21:28:13 +0100

    0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,15 +8,17 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *ht;
-	unsigned long int i = 0;
+	hash_table_t *ht = NULL;
+	unsigned long int i;
+
+	if (size < 1)
+		return (NULL);
 
 	ht = malloc(sizeof(hash_table_t));
 
 	if (ht == NULL)
 		return (NULL);
-	
-	ht->size = size;
+
 	ht->array = malloc(size * sizeof(hash_node_t *));
 
 	if (ht->array == NULL)
@@ -24,6 +26,8 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	for (i = 0; i < size; i++)
 		ht->array[i] = NULL;
+
+	ht->size = size;
 
 	return (ht);
 }
