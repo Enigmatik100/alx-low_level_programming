f0fbac1f22faed7efa0be25cae714da8917a01f9
enigmatik100 <paul.gangbadja@erugis.fr>
2022-07-03 19:59:11 +0100

    5-hash_table_print.c

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+/**
+ * hash_table_print - Function that prints a hash table.
+ * @ht: the hash table
+ * Return: anything
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *node;
+	unsigned long int i, display_comma = 0;
+
+
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		node = ht->array[i];
+		while (node != NULL)
+		{
+			if (display_comma == 1)
+				printf(", ");
+
+			printf("'%s: %s'", node->key, node->value);
+			node = node->next;
+			display_comma = 1;
+		}
+	}
+	printf("}\n");
+}
