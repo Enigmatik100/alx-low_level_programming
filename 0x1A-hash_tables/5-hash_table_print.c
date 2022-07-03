0c5084366683a8d0790cb0a33db91d94952e61c7
enigmatik100 <paul.gangbadja@erugis.fr>
2022-07-03 20:01:16 +0100

    5-hash_table_print.c

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -19,7 +19,7 @@ void hash_table_print(const hash_table_t *ht)
 			if (display_comma == 1)
 				printf(", ");
 
-			printf("'%s: %s'", node->key, node->value);
+			printf("'%s': '%s'", node->key, node->value);
 			node = node->next;
 			display_comma = 1;
 		}
